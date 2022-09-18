bd53cf7dda327f9cf0a4070869047030e987810c
Kin <kinlau529@gmail.com>
2022-09-19 07:32:11 +0800

    19/09/2022 07:32:09,

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import util
 list_of_repo = [
     "Early old works",
     "Git management",
+    "Python coding gym",
+    "Textual notes",
 ]
 
 list_of_location = [f".\{elt}" for elt in list_of_repo]
