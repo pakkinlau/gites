99aea8914adff8e9c26b3aba0af58150fdd97542
pakkinlau <kinlau529@gmail.com>
2023-11-01 02:45:18 +0800

    Automated add-commit-push. Datetime tag: 01/11/2023 02:45:18

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ package_data = {
 
 setup(
     name='gites',
-    version='0.4.4',
-    description='gites is the home of all developers. It provides bulk clone, bulk push, bulk pull etc. Contact me with my discord ID: pakkin.lau',
+    version='0.4.5',
+    description='gites is the home of all developers. It provides bulk clone, bulk push, bulk pull etc functions. Contact me with my discord ID: pakkin.lau',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Pak Kin LAU',
